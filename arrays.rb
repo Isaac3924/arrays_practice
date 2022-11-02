b008703eb47273a98de0b888ae833fe8d52289a3
Isaac Alter <i_alter@yahoo.com>
2022-11-02 14:01:36 -0600

    Adds 4 array varibles

--- arrays.rb
+++ arrays.rb
@@ -1,1 +1,4 @@
-food = ["cheese", "steak", "celery", "watermelon"]
+foods = ["cheese", "steak", "celery", "watermelon"]
+nums = [23, 51, 86, 94]
+floats = [3.14, 4.69, 38.99, 102.35]
+bools = [true, false, false, true]
